10260353bae1810c2530c4e30102d710560ddb89
Temerold <tobbe@temerold.se>
2022-06-20 23:06:28 +0200

    Remove debug statement

--- deltatime_parser.py
+++ deltatime_parser.py
@@ -11,6 +11,3 @@ def delta_time_parser(txt):
         {"y": 31557600, "d": 86400, "h": 3600, "m": 60, "s": 1}[t] * int(a)
         for a, t in re.findall("(\d+)(.)", txt.lower())
     )
-
-
-print(delta_time_parser("1H"))
